712669e0654e8b89431506b099b01b1739b49603
Kathy Hudson <khitx@icloud.com>
2018-01-17 16:15:28 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,9 @@ def key_for_min_value(name_hash)
     return nil
   end
   min_value_key = ''
-  min_value = ''
+  min_value = nil
   name_hash.each do |name, value|
-    if value < min_value
+    if min_value != nil && value < min_value
       min_value_key = name
       min_value = value
     end
